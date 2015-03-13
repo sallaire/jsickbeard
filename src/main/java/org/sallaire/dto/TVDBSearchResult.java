591a25b7ca2235a3fd6b560d857795203cebbb0b
sallaire <allaire.sylvain@gmail.com>
2015-03-13 07:53:14 +0100

    Test appel TVDB

---
+++ src/main/java/org/sallaire/dto/TVDBSearchResult.java
@@ -0,0 +1,75 @@
+package org.sallaire.dto;
+
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Series")
+public class TVDBSearchResult {
+	private Long id;
+	private String language;
+	
+	private String name;
+	
+	private String overview;
+	
+	private String firstAired;
+	
+	private String network;
+	
+	private Long imdbId;
+	
+	
+	public TVDBSearchResult() {
+		super();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	@XmlElement(name = "SeriesName")
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@XmlElement(name = "Overview")
+	public String getOverview() {
+		return overview;
+	}
+	public void setOverview(String overview) {
+		this.overview = overview;
+	}
+	@XmlElement(name = "FirstAired")
+	public String getFirstAired() {
+		return firstAired;
+	}
+	public void setFirstAired(String firstAired) {
+		this.firstAired = firstAired;
+	}
+	@XmlElement(name = "Network")
+	public String getNetwork() {
+		return network;
+	}
+	public void setNetwork(String network) {
+		this.network = network;
+	}
+	
+	@XmlElement(name = "IMDB_ID")
+	public Long getImdbId() {
+		return imdbId;
+	}
+	public void setImdbId(Long imdbId) {
+		this.imdbId = imdbId;
+	}
+	
+}
---
+++ src/main/java/org/sallaire/dto/TVDBSearchResults.java
@@ -0,0 +1,25 @@
+package org.sallaire.dto;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "Data")
+public class TVDBSearchResults {
+	
+	List<TVDBSearchResult> results;
+
+	public TVDBSearchResults() {
+		super();
+	}
+
+	@XmlElement(name = "Series", type = TVDBSearchResult.class)
+	public List<TVDBSearchResult> getResults() {
+		return results;
+	}
+
+	public void setResults(List<TVDBSearchResult> results) {
+		this.results = results;
+	}
+}
