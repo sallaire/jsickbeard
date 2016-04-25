4b14c553c177fb7767ccdee50c768a02a85685c9
sallaire <allaire.sylvain@gmail.com>
2016-04-25 18:15:38 +0100

    Modif gestions logs

--- src/main/java/org/sallaire/service/provider/t411/T411Provider.java
+++ src/main/java/org/sallaire/service/provider/t411/T411Provider.java
@@ -41,224 +41,222 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class T411Provider implements IProvider {
 
-    public static final String ID = "t411";
-    public static final String USER = "user";
-    public static final String PASSWORD = "password";
+	public static final String ID = "t411";
+	public static final String USER = "user";
+	public static final String PASSWORD = "password";
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(T411Provider.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(T411Provider.class);
 
-    @Autowired
-    private T411Configuration configuration;
+	@Autowired
+	private T411Configuration configuration;
 
-    @Autowired
-    private RegexFilterConfiguration regex;
+	@Autowired
+	private RegexFilterConfiguration regex;
 
-    private RestTemplate restTemplate;
+	private RestTemplate restTemplate;
 
-    private AuthenticationInterceptor authenticationInterceptor = new AuthenticationInterceptor();
+	private AuthenticationInterceptor authenticationInterceptor = new AuthenticationInterceptor();
 
-    private String user, password;
+	private String user, password;
 
-    @PostConstruct
-    public void init() {
-	MDC.put(PROVIDER, ID);
-	LOGGER.debug("Initializing T411 Provider");
-	// Initialize rest client
-	LOGGER.debug("Initializing T411 rest client");
-	restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+	@PostConstruct
+	public void init() {
+		MDC.put(PROVIDER, ID);
+		LOGGER.debug("Initializing T411 Provider");
+		// Initialize rest client
+		LOGGER.debug("Initializing T411 rest client");
+		restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
 
-	LOGGER.debug("Initializing custom T411 converter");
-	MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
-	List<MediaType> supportedMediaTypes = new ArrayList<>(jsonConverter.getSupportedMediaTypes());
-	supportedMediaTypes.add(new MediaType("text", "html"));
-	jsonConverter.setSupportedMediaTypes(supportedMediaTypes);
-	restTemplate.getMessageConverters().add(jsonConverter);
-	LOGGER.debug("Custom deluge converter initialized");
+		LOGGER.debug("Initializing custom T411 converter");
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+		List<MediaType> supportedMediaTypes = new ArrayList<>(jsonConverter.getSupportedMediaTypes());
+		supportedMediaTypes.add(new MediaType("text", "html"));
+		jsonConverter.setSupportedMediaTypes(supportedMediaTypes);
+		restTemplate.getMessageConverters().add(jsonConverter);
+		LOGGER.debug("Custom deluge converter initialized");
 
-	LOGGER.debug("Setting T411 interceptor");
-	restTemplate.setInterceptors(Arrays.asList(authenticationInterceptor));
-	LOGGER.debug("T411 interceptor set");
+		LOGGER.debug("Setting T411 interceptor");
+		restTemplate.setInterceptors(Arrays.asList(authenticationInterceptor));
+		LOGGER.debug("T411 interceptor set");
 
-	LOGGER.debug("T411 Provider initialized");
-    }
+		LOGGER.debug("T411 Provider initialized");
+		MDC.remove(PROVIDER);
+	}
 
-    @Override
-    public Torrent findEpisode(Collection<String> names, String audioLang, Integer season, Integer number, Quality quality, List<String> excludedFiles)
-	    throws IOException {
-	MDC.put(PROVIDER, ID);
-	LOGGER.debug("Authenticating to T411");
-	authenticationInterceptor.setToken(login());
+	@Override
+	public Torrent findEpisode(Collection<String> names, String audioLang, Integer season, Integer number, Quality quality, List<String> excludedFiles) throws IOException {
+		MDC.put(PROVIDER, ID);
+		LOGGER.debug("Authenticating to T411");
+		authenticationInterceptor.setToken(login());
 
-	List<SearchResult> results = null;
-	for (String name : names) {
-	    for (Integer option : Option.getOptionsToCheck()) {
-		results = findEpisode(name, audioLang, season, number, quality, option, excludedFiles);
+		List<SearchResult> results = null;
+		for (String name : names) {
+			for (Integer option : Option.getOptionsToCheck()) {
+				results = findEpisode(name, audioLang, season, number, quality, option, excludedFiles);
+				if (CollectionUtils.isNotEmpty(results)) {
+					LOGGER.debug("Results found for episode, stopping search");
+					break;
+				}
+			}
+		}
+
 		if (CollectionUtils.isNotEmpty(results)) {
-		    LOGGER.debug("Results found for episode, stopping search");
-		    break;
+			try {
+				SearchResult torrentToRetrieve = findBestResult(results);
+				LOGGER.debug("Torrent {} will be downloaded", torrentToRetrieve.getName());
+				URIBuilder builder = new URIBuilder().setScheme(configuration.getProtocol()).setHost(configuration.getHost()).setPath("/torrents/download/" + torrentToRetrieve.getId());
+				Path torrent = Files.createTempFile(null, null);
+				LOGGER.debug("Downloading torrent with url [{}] to [{}]", builder.build(), torrent);
+				byte[] bytes = restTemplate.getForObject(builder.build(), byte[].class);
+				Files.write(torrent, bytes);
+				return new Torrent(torrentToRetrieve.getName(), torrent);
+			} catch (URISyntaxException e) {
+				throw new IOException(e);
+			}
 		}
-	    }
+		MDC.remove(PROVIDER);
+		return null;
+
 	}
 
-	if (CollectionUtils.isNotEmpty(results)) {
-	    try {
-		SearchResult torrentToRetrieve = findBestResult(results);
-		LOGGER.debug("Torrent {} will be downloaded", torrentToRetrieve.getName());
-		URIBuilder builder = new URIBuilder().setScheme(configuration.getProtocol()).setHost(configuration.getHost())
-			.setPath("/torrents/download/" + torrentToRetrieve.getId());
-		Path torrent = Files.createTempFile(null, null);
-		LOGGER.debug("Downloading torrent with url [{}] to [{}]", builder.build(), torrent);
-		byte[] bytes = restTemplate.getForObject(builder.build(), byte[].class);
-		Files.write(torrent, bytes);
-		return new Torrent(torrentToRetrieve.getName(), torrent);
-	    } catch (URISyntaxException e) {
-		throw new IOException(e);
-	    }
+	private SearchResult findBestResult(List<SearchResult> results) {
+		if (CollectionUtils.isEmpty(results)) {
+			return null;
+		}
+		if (results.size() > 1) {
+			LOGGER.debug("More than one result has been found, picking best one");
+			/**
+			 * We have to take the 'best' one : proper / verified / seeders
+			 */
+			LOGGER.debug("Searching PROPER episodes");
+			Pattern pattern = Pattern.compile("PROPER", Pattern.CASE_INSENSITIVE);
+			List<SearchResult> filtered = results.stream().filter(s -> pattern.matcher(s.getName()).matches()).collect(Collectors.toList());
+			if (!filtered.isEmpty()) {
+				LOGGER.debug("{} PROPER episodes found", filtered.size());
+				results = filtered;
+			}
+			if (results.size() == 1) {
+				return results.get(0);
+			} else {
+				LOGGER.debug("Searching verified episodes");
+				filtered = results.stream().filter(s -> s.isVerified()).collect(Collectors.toList());
+			}
+			if (!filtered.isEmpty()) {
+				LOGGER.debug("{} verified episodes found", filtered.size());
+				results = filtered;
+			}
+			if (results.size() == 1) {
+				return results.get(0);
+			} else {
+				LOGGER.debug("returning episode with most seeders");
+				return results.stream().max(Comparator.comparing(SearchResult::getSeeders)).get();
+			}
+		} else {
+			LOGGER.debug("Only one result has been found, picking it by default");
+			return results.get(0);
+		}
 	}
 
-	return null;
+	private List<SearchResult> findEpisode(String name, String audioLang, Integer season, Integer number, Quality quality, Integer option, List<String> excludedFiles) throws IOException {
+		URIBuilder builder = new URIBuilder().setScheme(configuration.getProtocol()).setHost(configuration.getHost()).setPath(configuration.getSearchPath() + name) //
+				.addParameter(configuration.getCategoryKey(), configuration.getCategory()) //
+				.addParameter(configuration.getSubCategoryKey(), configuration.getSubCategory());
 
-    }
-
-    private SearchResult findBestResult(List<SearchResult> results) {
-	if (CollectionUtils.isEmpty(results)) {
-	    return null;
-	}
-	if (results.size() > 1) {
-	    LOGGER.debug("More than one result has been found, picking best one");
-	    /**
-	     * We have to take the 'best' one : proper / verified / seeders
-	     */
-	    LOGGER.debug("Searching PROPER episodes");
-	    Pattern pattern = Pattern.compile("PROPER", Pattern.CASE_INSENSITIVE);
-	    List<SearchResult> filtered = results.stream().filter(s -> pattern.matcher(s.getName()).matches()).collect(Collectors.toList());
-	    if (!filtered.isEmpty()) {
-		LOGGER.debug("{} PROPER episodes found", filtered.size());
-		results = filtered;
-	    }
-	    if (results.size() == 1) {
-		return results.get(0);
-	    } else {
-		LOGGER.debug("Searching verified episodes");
-		filtered = results.stream().filter(s -> s.isVerified()).collect(Collectors.toList());
-	    }
-	    if (!filtered.isEmpty()) {
-		LOGGER.debug("{} verified episodes found", filtered.size());
-		results = filtered;
-	    }
-	    if (results.size() == 1) {
-		return results.get(0);
-	    } else {
-		LOGGER.debug("returning episode with most seeders");
-		return results.stream().max(Comparator.comparing(SearchResult::getSeeders)).get();
-	    }
-	} else {
-	    LOGGER.debug("Only one result has been found, picking it by default");
-	    return results.get(0);
-	}
-    }
-
-    private List<SearchResult> findEpisode(String name, String audioLang, Integer season, Integer number, Quality quality, Integer option,
-	    List<String> excludedFiles) throws IOException {
-	URIBuilder builder = new URIBuilder().setScheme(configuration.getProtocol()).setHost(configuration.getHost())
-		.setPath(configuration.getSearchPath() + name) //
-		.addParameter(configuration.getCategoryKey(), configuration.getCategory()) //
-		.addParameter(configuration.getSubCategoryKey(), configuration.getSubCategory());
-
-	if (Option.NUMBER.isActivated(option)) {
-	    builder.addParameter(configuration.getSeasonKey(), String.valueOf(configuration.getSeason(season))) //
-		    .addParameter(configuration.getEpisodeKey(), String.valueOf(configuration.getEpisode(number)));
-	} else {
-	    builder.setPath("/torrents/search/" + String.format(regex.getEpisodeFormat(), name, season, number));
-	}
-
-	if (Option.LANG.isActivated(option)) {
-	    List<String> params = configuration.getLangValue(audioLang);
-	    for (String param : params) {
-		builder.addParameter(configuration.getLangKey(), param);
-	    }
-	}
-
-	if (Option.QUALITY.isActivated(option)) {
-	    List<String> params = configuration.getQualityValue(quality);
-	    for (String param : params) {
-		builder.addParameter(configuration.getQualityKey(), param);
-	    }
-	}
-
-	try {
-	    LOGGER.info("Searching episode with request [{}]", builder.build());
-	    SearchResults results = restTemplate.getForObject(builder.build(), SearchResults.class);
-
-	    if (CollectionUtils.isNotEmpty(results.getTorrents())) {
-		LOGGER.info("{} found for request", results.getTorrents().size());
-		if (LOGGER.isDebugEnabled()) {
-		    LOGGER.debug("Torrents found with request :");
-		    results.getTorrents().stream().forEach(t -> LOGGER.debug(" - {}", t.getName()));
+		if (Option.NUMBER.isActivated(option)) {
+			builder.addParameter(configuration.getSeasonKey(), String.valueOf(configuration.getSeason(season))) //
+					.addParameter(configuration.getEpisodeKey(), String.valueOf(configuration.getEpisode(number)));
+		} else {
+			builder.setPath("/torrents/search/" + String.format(regex.getEpisodeFormat(), name, season, number));
 		}
 
-		// Filter not wanted files
-		results.getTorrents().stream().filter(t -> {
-		    if (excludedFiles.contains(t.getName())) {
-			LOGGER.debug("Exclude result {} because it's an unwanted one", t.getName());
-			return true;
-		    } else {
-			return false;
-		    }
-		});
+		if (Option.LANG.isActivated(option)) {
+			List<String> params = configuration.getLangValue(audioLang);
+			for (String param : params) {
+				builder.addParameter(configuration.getLangKey(), param);
+			}
+		}
 
-		// Now we have to filter results according to
-		// quality/language/episode name
-		LOGGER.info("Filter results for episode name [{}]", name);
-		results.setTorrents(filterResults(results.getTorrents(), name, quality, audioLang, season, number));
-		LOGGER.info("{} results after filter", results.getTorrents().size());
-	    } else {
-		LOGGER.info("No result found for request");
-	    }
-	    return results.getTorrents();
-	} catch (URISyntaxException e) {
-	    throw new IOException(e);
+		if (Option.QUALITY.isActivated(option)) {
+			List<String> params = configuration.getQualityValue(quality);
+			for (String param : params) {
+				builder.addParameter(configuration.getQualityKey(), param);
+			}
+		}
+
+		try {
+			LOGGER.info("Searching episode with request [{}]", builder.build());
+			SearchResults results = restTemplate.getForObject(builder.build(), SearchResults.class);
+
+			if (CollectionUtils.isNotEmpty(results.getTorrents())) {
+				LOGGER.info("{} found for request", results.getTorrents().size());
+				if (LOGGER.isDebugEnabled()) {
+					LOGGER.debug("Torrents found with request :");
+					results.getTorrents().stream().forEach(t -> LOGGER.debug(" - {}", t.getName()));
+				}
+
+				// Filter not wanted files
+				results.getTorrents().stream().filter(t -> {
+					if (excludedFiles.contains(t.getName())) {
+						LOGGER.debug("Exclude result {} because it's an unwanted one", t.getName());
+						return true;
+					} else {
+						return false;
+					}
+				});
+
+				// Now we have to filter results according to
+				// quality/language/episode name
+				LOGGER.info("Filter results for episode name [{}]", name);
+				results.setTorrents(filterResults(results.getTorrents(), name, quality, audioLang, season, number));
+				LOGGER.info("{} results after filter", results.getTorrents().size());
+			} else {
+				LOGGER.info("No result found for request");
+			}
+			return results.getTorrents();
+		} catch (URISyntaxException e) {
+			throw new IOException(e);
+		}
 	}
-    }
 
-    private List<SearchResult> filterResults(List<SearchResult> results, String showName, Quality quality, String lang, Integer season, Integer number) {
-	return results.stream().filter(t -> {
-	    return regex.matchEpisode(t.getName(), showName, season, number, quality, lang);
-	}).collect(Collectors.toList());
-    }
-
-    private String login() throws IOException {
-	LOGGER.debug("Authenticating to T411");
-
-	MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
-	map.add("username", user);
-	map.add("password", password);
-	LOGGER.debug("Parameters for T411 authentication : {} -> {}", user, StringUtils.repeat("X", password.length()));
-
-	LOGGER.debug("Posting request to T411 authentication [{}]", configuration.getAuthPath());
-	return restTemplate.postForObject(configuration.getAuthPath(), map, Authorization.class).getToken();
-	// TODO tester retour en cas de mauvais password
-    }
-
-    @Override
-    public String getId() {
-	return ID;
-    }
-
-    @Override
-    public void configurationChanged(Map<String, String> parameters) {
-	MDC.put(PROVIDER, ID);
-	if (MapUtils.isNotEmpty(parameters)) {
-	    LOGGER.debug("T411 configuration changed, update parameters : ");
-	    user = parameters.get(USER);
-	    LOGGER.debug(" user = {}", user);
-	    password = parameters.get(PASSWORD);
-	    LOGGER.debug(" password = {}", StringUtils.repeat("X", password.length()));
-	} else {
-	    LOGGER.debug("Cleaning T411 configuration");
-	    user = "";
-	    password = "";
+	private List<SearchResult> filterResults(List<SearchResult> results, String showName, Quality quality, String lang, Integer season, Integer number) {
+		return results.stream().filter(t -> {
+			return regex.matchEpisode(t.getName(), showName, season, number, quality, lang);
+		}).collect(Collectors.toList());
 	}
-    }
+
+	private String login() throws IOException {
+		LOGGER.debug("Authenticating to T411");
+
+		MultiValueMap<String, Object> map = new LinkedMultiValueMap<>();
+		map.add("username", user);
+		map.add("password", password);
+		LOGGER.debug("Parameters for T411 authentication : {} -> {}", user, StringUtils.repeat("X", password.length()));
+
+		LOGGER.debug("Posting request to T411 authentication [{}]", configuration.getAuthPath());
+		return restTemplate.postForObject(configuration.getAuthPath(), map, Authorization.class).getToken();
+		// TODO tester retour en cas de mauvais password
+	}
+
+	@Override
+	public String getId() {
+		return ID;
+	}
+
+	@Override
+	public void configurationChanged(Map<String, String> parameters) {
+		MDC.put(PROVIDER, ID);
+		if (MapUtils.isNotEmpty(parameters)) {
+			LOGGER.debug("T411 configuration changed, update parameters : ");
+			user = parameters.get(USER);
+			LOGGER.debug(" user = {}", user);
+			password = parameters.get(PASSWORD);
+			LOGGER.debug(" password = {}", StringUtils.repeat("X", password.length()));
+		} else {
+			LOGGER.debug("Cleaning T411 configuration");
+			user = "";
+			password = "";
+		}
+		MDC.remove(PROVIDER);
+	}
 
 }
